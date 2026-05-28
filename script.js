f3f0b3800452b0f66ecf6d847bdbae62c3e31890
Abhishek1IT <abhishekkumargupta303@gmail.com>
2026-05-28 13:37:56 +0000

    Modify

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ const words = [
   },
   {
     word: "punjabi",
-    hint: "Language spoken in Punjab region",
+    hint: "Language spoken in North-West region",
   },
   {
     word: "gujarati",
-    hint: "Language spoken in Gujarat region",
+    hint: "Language spoken in West region",
   },
   {
     word: "marathi",
-    hint: "Language spoken in Maharashtra region",
+    hint: "Language spoken in Western Peninsula region",
   },
   {
     word: "bengali",
-    hint: "Language spoken in West Bengal region",
+    hint: "Language spoken in Eastern region of Indian subcontinent",
   },
   {
     word: "tamil",
-    hint: "Language spoken in Tamil Nadu region",
+    hint: "Language spoken in Southernmost Part of India",
   },
   {
     word: "telugu",
-    hint: "Language spoken in Andhra Pradesh regions",
+    hint: "Language spoken in Southeastern Part of India",
   },
   {
     word: "kannada",
-    hint: "Language spoken in Karnataka region",
+    hint: "Language spoken in Southwestern Part of India",
   },
 ];
 const randomData = words[Math.floor(Math.random() * words.length)];
@@ -63,6 +63,7 @@ const messageElement = document.getElementById("message");
 const attemptsElement = document.getElementById("attempts");
 
 function displayWord() {
+
   wordElement.innerText = displayLetters.join(" ");
 }
 
@@ -71,28 +72,35 @@ displayWord();
 attemptsElement.innerText = `Attempts Left: ${attempts}`;
 
 guessBtn.addEventListener("click", function () {
+
   const guessedLetter = letterInput.value.toLowerCase();
 
   letterInput.value = "";
 
   if (guessedLetter === "") {
+
     return;
   }
 
   let totalCount = 0;
+
   let revealedCount = 0;
 
   for (let i = 0; i < word.length; i++) {
+
     if (word[i] === guessedLetter) {
+
       totalCount++;
     }
 
     if (displayLetters[i] === guessedLetter) {
+
       revealedCount++;
     }
   }
 
   if (totalCount > 0 && totalCount === revealedCount) {
+
     messageElement.innerText = "Letter already guessed!";
 
     return;
@@ -103,7 +111,9 @@ guessBtn.addEventListener("click", function () {
   let found = false;
 
   for (let i = 0; i < word.length; i++) {
+
     if (word[i] === guessedLetter && displayLetters[i] === "_") {
+
       displayLetters[i] = guessedLetter;
 
       found = true;
@@ -115,9 +125,13 @@ guessBtn.addEventListener("click", function () {
   attempts--;
 
   if (found) {
+
     messageElement.innerText = "Correct Guess!";
+
   } else {
+
     messageElement.innerText = "Wrong Guess!";
+
   }
 
   attemptsElement.innerText = `Attempts Left: ${attempts}`;
@@ -128,15 +142,19 @@ guessBtn.addEventListener("click", function () {
 });
 
 function checkGame() {
+
   let revealedCount = 0;
 
   for (let letter of displayLetters) {
+
     if (letter !== "_") {
+
       revealedCount++;
     }
   }
 
   if (revealedCount >= winningTarget) {
+
     messageElement.innerText = "Congratulations! You Won!";
 
     wordElement.innerText = word;
@@ -145,10 +163,13 @@ function checkGame() {
 
     guessBtn.disabled = true;
 
+    restartBtn.style.display = "inline-block";
+
     return;
   }
 
   if (attempts <= 0) {
+
     messageElement.innerText = `Game Over! Word was: ${word}`;
 
     wordElement.innerText = word;
@@ -156,5 +177,15 @@ function checkGame() {
     letterInput.disabled = true;
 
     guessBtn.disabled = true;
+
+    restartBtn.style.display = "inline-block";
+
   }
+
 }
+
+restartBtn.addEventListener("click", function () {
+
+  location.reload();
+
+});
